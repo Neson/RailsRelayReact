e4e109a32140a84a0a6fb17a8ef399a087949183
Neson <neson@dex.tw>
2016-07-17 23:16:44 +0800

    scaffold post/comment

---
+++ GraphQLRails/app/views/post/comments/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post_comment, :id, :content, :created_at, :updated_at
--- GraphQLRails/config/routes.rb
+++ GraphQLRails/config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   post :graphql, to: "graphql#query"
   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
