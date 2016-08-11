a5a102f2d5f526f8d38bce2eab3c9c609b93dfba
Neson <neson@dex.tw>
2016-08-11 10:35:43 +0800

    write the PostTitle component

--- RelayReactNative/components/PostTitle.js
+++ RelayReactNative/components/PostTitle.js
@@ -5,6 +5,7 @@
 import React, { Component } from 'react';
 import Relay from 'react-relay';
 import {
+  StyleSheet,
   View,
   Text
 } from 'react-native';
@@ -14,19 +15,34 @@ class PostTitle extends Component {
     let { post } = this.props;
 
     return (
-      <View>
-        <Text>{post.subject}</Text>
+      <View style={styles.title}>
+        <Text style={styles.titleText}>{post.subject}</Text>
       </View>
     );
   }
 }
 
-module.exports = Relay.createContainer(PostTitle, {
+export default Relay.createContainer(PostTitle, {
   fragments: {
     post: () => Relay.QL`
       fragment on Post {
         subject
       }
     `
+  }
+});
+
+const styles = StyleSheet.create({
+  title: {
+    marginVertical: 5,
+    marginHorizontal: 2,
+    borderLeftWidth: 8,
+    borderLeftColor: '#000',
+    height: 22,
+    paddingHorizontal: 7,
+    justifyContent: 'center'
+  },
+  titleText: {
+    fontSize: 18
   }
 });
